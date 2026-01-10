7a19b98cf0d63ad91c984b5557574350809f0cfc
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-10 22:02:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,60 +1,30 @@
 package main
 
 import (
-	"image/color"
-	"log"
 	"os"
 
 	"gioui.org/app"
 	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/widget"
 	"gioui.org/widget/material"
 )
 
-// ==================================
-func run(window *app.Window) error {
-	var nameInput widget.Editor //textbox
-	nameInput.SingleLine = true
-	nameInput.Submit = true //catches Enter button
-	theme := material.NewTheme()
-	var ops op.Ops
-	for {
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			// This graphics context is used for managing the rendering state.
-			gtx := app.NewContext(&ops, e)
-
-			// Define an large label with an appropriate text:
-			title := material.H1(theme, "GPU Finder")
-
-			// Change the color of the label.
-			maroon := color.NRGBA{R: 100, G: 50, B: 50, A: 255}
-			title.Color = maroon
-
-			// Change the position of the label.
-			title.Alignment = text.Middle
-
-			// Draw the label to the graphics context.
-			title.Layout(gtx)
-
-			// Pass the drawing operations to the GPU.
-			e.Frame(gtx.Ops)
-		}
-	}
-}
-
-// ==================================
 func main() {
-	go func() {
-		window := new(app.Window) //creating window
-		err := run(window)        //calling run
-		if err != nil {           //if run function returns error
-			log.Fatal(err)
+	go func() { //creating go routing func,routing func in GO is a thread
+		w := new(app.Window)      //creating a window
+		th := material.NewTheme() //creating a theme th is a variable
+		var ops op.Ops            //holds the operations,it is a recording buffer
+		for {                     //this loops listens to the window`s events
+			switch e := w.Event().(type) { //based on the vents types the code will dicide what to do using switch/cae
+			case app.DestroyEvent: //same as distructor in C++
+				os.Exit(0)
+			case app.FrameEvent: //is being called when the redraw is needed,updates the window
+				gtx := app.NewContext(&ops, e)          //e holds the window size f examp. ops is a list of operations
+				material.Label(th, 16, "H").Layout(gtx) //th - theme,16 - text size,"" - text in the label
+				//.Layout(gtx) is drawing the text
+				e.Frame(gtx.Ops) //draws called operations
+			}
+			//============================
 		}
-		os.Exit(0)
 	}()
-	app.Main()
+	app.Main() //keeps the Event loop alive
 }
